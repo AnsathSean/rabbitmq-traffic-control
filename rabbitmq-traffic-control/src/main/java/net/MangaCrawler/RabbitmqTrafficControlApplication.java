b9b0c36af5bc757503cf287b4d28f5206c7a2be0
Ansathsean <eric267213@hotmail.com>
2025-07-24 22:04:50 +0800

    第一次Push

---
+++ rabbitmq-traffic-control/src/main/java/net/MangaCrawler/RabbitmqTrafficControlApplication.java
@@ -0,0 +1,13 @@
+package net.MangaCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqTrafficControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqTrafficControlApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-traffic-control/src/test/java/net/MangaCrawler/RabbitmqTrafficControlApplicationTests.java
@@ -0,0 +1,13 @@
+package net.MangaCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqTrafficControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
